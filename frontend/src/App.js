e9d7399c78ffe91b9f6056f3f111bb5257bd7a9c
Richard <richard.lin.047@berkeley.edu>
2018-11-03 19:06:01 -0700

    add mapping of listings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import Listings from './Listings';
+import AllListings from './AllListings';
+
 import SearchBar from './SearchBar';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <SearchBar />
-        < Listings 
-        image_url = {
-            'http://www.returnofkings.com/wp-content/uploads/2014/10/study-den.jpg'
-          }
-        price = {'$294'}  
-        reviews = {'4.9/5'}
+        <AllListings
         />
       </div>
     );
--- frontend/src/Listings.js
+++ frontend/src/Listings.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import './Listings.css';
 
 class Listings extends Component {
   render() {
       const {image_url, price, reviews } = this.props;
     return (
-      <div className="Listings">
-        <h1>
-          George's cozy den Perfect for studying!
-        </h1>
-        <ul>
-          <li> <img src = {image_url} alt='Broken'/> </li>
-          <li> Price: {price} </li>
-          <li> Reviews: {reviews} </li>
-        </ul>
+      <div className="ListingsContainer">
+        <div className="ListingImageContainer">
+          <img src = {image_url} alt='Broken' className="ListingImage"/>
+        </div>
+        <div className="ListingContent">
+          <h2 className="ListingContentType">
+            George's cozy den Perfect for studying!
+          </h2>
+          <h1 className="ListingContentTitle">
+            Large private bedroom
+          </h1>
+          <p>Amenities here!</p>
+        </div>
+        <div className="ListingPrice">
+          <p> Price: {price} </p>
+          <p> Reviews: {reviews} </p>
+        </div>
       </div>
     );
   }
